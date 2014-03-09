a61cae269bdf34fdd4a900a3b7279ff05f2b43cf
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2014-03-09 16:45:19 +0000

    fixed compilation issues

--- src/main/java/com/zyin/zyinhud/command/CommandZyinHUDOptions.java
+++ src/main/java/com/zyin/zyinhud/command/CommandZyinHUDOptions.java
@@ -5,25 +5,32 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import com.zyin.zyinhud.gui.GuiZyinHUDOptions;
 
-public class CommandZyinHUDOptions  extends CommandBase
+public class CommandZyinHUDOptions extends CommandBase
 {
-	private static Minecraft mc = Minecraft.getMinecraft();
-	
-	@Override
-	public String getCommandName()
-	{
-		return "zyinhud";
-	}
 
-	@Override
-	public String getCommandUsage(ICommandSender iCommandSender)
-	{
-		return "commands.zyinhud.usage";
-	}
+    private static Minecraft mc = Minecraft.getMinecraft();
 
-	@Override
-	public void processCommand(ICommandSender iCommandSender, String[] parameters)
-	{
-		mc.displayGuiScreen(new GuiZyinHUDOptions(null));
-	}
+    @Override
+    public String getCommandName()
+    {
+        return "zyinhud";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender iCommandSender)
+    {
+        return "commands.zyinhud.usage";
+    }
+
+    @Override
+    public void processCommand(ICommandSender iCommandSender, String[] parameters)
+    {
+        mc.displayGuiScreen(new GuiZyinHUDOptions(null));
+    }
+
+    @Override
+    public int compareTo(Object t)
+    {
+        return ( (CommandZyinHUDOptions) t ).getCommandName().compareTo( getCommandName() );
+    }
 }
